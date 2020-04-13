a810ece06048036985d3675958d78ad9c9e4bffc
omerbulutx <omer.bulut@teamdefinex.com>
2020-04-13 16:21:37 +0300

    Updates

---
+++ precache-manifest.113cf894fa4830105124713cc0b872f9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54322d77e790ce8da9106e77e72a2ba6",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "c65e777dd9e94a0339e3",
+    "url": "/MyPortfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "a880e3ae5f2dfdae6f49",
+    "url": "/MyPortfolio/static/css/main.4114cde6.chunk.css"
+  },
+  {
+    "revision": "c65e777dd9e94a0339e3",
+    "url": "/MyPortfolio/static/js/2.e4ad27a3.chunk.js"
+  },
+  {
+    "revision": "eaa608c5c30088e600c73b82a902b319",
+    "url": "/MyPortfolio/static/js/2.e4ad27a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a880e3ae5f2dfdae6f49",
+    "url": "/MyPortfolio/static/js/main.9669fc20.chunk.js"
+  },
+  {
+    "revision": "9130e4db92fd4f4c6b75",
+    "url": "/MyPortfolio/static/js/runtime-main.0dcf000b.js"
+  },
+  {
+    "revision": "d8598402977e138afc5260e3495393d5",
+    "url": "/MyPortfolio/static/media/omer.d8598402.pdf"
+  },
+  {
+    "revision": "9c24e7edfee559107843f4d416ed8f2f",
+    "url": "/MyPortfolio/static/media/profile.9c24e7ed.JPG"
+  }
+]);
